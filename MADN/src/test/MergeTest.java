cd64c750e3a0ed127d68c22861183b9512efb33a
Verena <Verena@Verena-PC>
2015-03-17 16:18:35 +0100

    test merge verena

--- MADN/src/test/MergeTest.java
+++ MADN/src/test/MergeTest.java
@@ -9,6 +9,7 @@ public class MergeTest {
 		System.out.println("Hier schreibt Verena..");
 		System.out.println("Abgabe");
 		System.out.println("Hier nochmal Daniel");
+		System.out.println("schon wieder die Verena");
 
 	}
 
