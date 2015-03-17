c792193b233fa7822260a8043862516651f71c55
DanielAyen <Daniel.Ayen@Student.Reutlingen-University.DE>
2015-03-17 13:55:21 +0100

    Daniel MergeTest

--- MADN/src/test/MergeTest.java
+++ MADN/src/test/MergeTest.java
@@ -5,6 +5,7 @@ public class MergeTest {
 	public static void main(String[] args) {
 		System.out.println("Hallo ich bin Sandra");
 		System.out.println("Hier schreibt Verena..");
+		System.out.println("Daniel testet..");
 
 	}
 
