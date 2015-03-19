b8b9843fadaa744b0a12d324c41aaef5ec9cc29c
DanielAyen <Daniel.Ayen@Student.Reutlingen-University.DE>
2015-03-19 16:42:53 +0100

    Spielfeld bearbeitet, Doc dazu...

--- MADN/src/Spielfeld.java
+++ MADN/src/Spielfeld.java
@@ -9,7 +9,11 @@ import java.util.ArrayList;
 
 public class Spielfeld {
 
-	ArrayList<Object> SF = new ArrayList<Object>();
+	/**
+	 * FeldID ArrayList für alle Spielfelder
+	 */
+
+	private ArrayList<Object> FeldID = new ArrayList<Object>();
 
 	/**
 	 * Hier wird jedem Feld eine ID (ein Name+Nummer) zugewiesen. Fuegt
@@ -17,6 +21,27 @@ public class Spielfeld {
 	 * 
 	 * @param i
 	 *            Zaehlt die Felder hoch.
+	 * 
+	 * @param SR
+	 *            Startfelder fuer Rot
+	 * @param SB
+	 *            Startfelder fuer Blau
+	 * @param SY
+	 *            Startfelder fuer Gelb
+	 * @param SG
+	 *            Startfelder fuer Gruen
+	 * 
+	 * @param x
+	 *            Normales Feld
+	 * 
+	 * @param ER
+	 *            Endfeld fuer Rot
+	 * @param EB
+	 *            Endfeld fuer Blau
+	 * @param EY
+	 *            Endfeld fuer Gelb
+	 * @param EG
+	 *            Endfeld fuer Gruen
 	 */
 
 	public void erstelleFeld() {
@@ -24,65 +49,55 @@ public class Spielfeld {
 		// Startfelder
 		// Felder RED
 		for (int i = 1; i <= 4; i++) {
-			SF.add("SR" + i);
+			FeldID.add("SR" + i);
 		}
 		// Felder BLUE
 		for (int i = 1; i <= 4; i++) {
-			SF.add("SB1" + i);
+			FeldID.add("SB1" + i);
 		}
 		// Felder YELLOW
 		for (int i = 1; i <= 4; i++) {
-			SF.add("SY1" + i);
+			FeldID.add("SY1" + i);
 		}
 		// Felder GREEN
 		for (int i = 1; i <= 4; i++) {
-			SF.add("SG1" + i);
+			FeldID.add("SG1" + i);
 		}
 
 		// Felder Normal
-		for (int i = 1; i <= 40; i++) {
+		for (int x = 1; x <= 40; x++) {
 
-			switch (i) {
-			case 1:
-				SF.add("Feld_SR" + i);// Feld Start1
-
-			case 11:
-				SF.add("Feld_SB" + i);// Feld Start11
-
-			case 21:
-				SF.add("Feld_SG" + i);// Feld Start21
-
-			case 31:
-				SF.add("Feld_SY" + i);// Feld Start31
-
-			default:
-				SF.add("Feld" + i); // normale Felder
-			}
+			FeldID.add(x); //
 		}
 
 		// Felder Ziel
 		for (int i = 1; i <= 4; i++) { // Zielfelder Rot1-4
-			SF.add("ER" + i);
+			FeldID.add("ER" + i);
 
 		}
 		for (int i = 1; i <= 4; i++) { // Zielfelder Blue1-4
-			SF.add("EB1" + i);
+			FeldID.add("EB1" + i);
 		}
 		for (int i = 1; i <= 4; i++) { // Zielfelder Yellow1-4
-			SF.add("EY1" + i);
+			FeldID.add("EY1" + i);
 		}
 		for (int i = 1; i <= 4; i++) { // Zielfelder Green1-4
-			SF.add("EG1" + i);
+			FeldID.add("EG1" + i);
 		}
 
 	}
-
+	/**
+		 * 
+		 */
 	/*
-	 * public void KenntFigur(ArrayList<Object> SF, Spielfigur){ 
-	 * int x;
-	 *  int temp;
-	 *   SF.get(x); x=temp; SF.add(x,Spielfigur); 
-	 *   }
+	 * public void KenntFigur(ArrayList<Object> SF, Spielfigur){ int x; int
+	 * temp; FeldID.get(x); x=temp; FeldID.add(x,Spielfigur);
+	 * 
+	 * }
+	 * 
+	 * public Object GetFeld(ArrayList<Object> SF,x){
+	 * 
+	 * return FeldID.get(x); }
 	 */
 
 }
