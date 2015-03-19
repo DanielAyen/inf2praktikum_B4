0e920afa0bdf51156371c268527bdbc4cfde313d
Elenis8 <eleni.stavrakis@student.reutlingen-university.de>
2015-03-19 16:54:11 +0100

    Änderung

--- MADN/src/Spielbrett.java
+++ MADN/src/Spielbrett.java
@@ -1,17 +1,35 @@
 
 public class Spielbrett {
 
-	Spielfigur []   spielbrettArray;
-	Spielfigur [][] startFeldArray;
-	Spielfigur [][] zielFeldArray;
+	private Spielfeld feld;
 	
-	public Spielbrett (){
-		spielbrettArray = new Spielfigur[40];
-		startFeldArray = new Spielfigur[4][4];
-		zielFeldArray = new Spielfigur[4][4];
+	private Spielfigur []   spielbrettArray = new Spielfigur [40];
+	private Spielfigur [][] startFeldArray = new Spielfigur [4][4];
+	private Spielfigur [][] zielFeldArray = new Spielfigur [4][4];
+	
+	public Spielbrett(Spielfeld feld, Spielfigur[] spielbrettArray,Spielfigur[][] startFeldArray, Spielfigur[][] zielFeldArray) {
+
+		this.feld = feld;
+		this.spielbrettArray = spielbrettArray;
+		this.startFeldArray = startFeldArray;
+		this.zielFeldArray = zielFeldArray;
 	}
-	
-	
+
+
+	public Spielfigur[] getSpielbrettArray() {
+		return spielbrettArray;
+	}
+
+
+	public Spielfigur[][] getStartFeldArray() {
+		return startFeldArray;
+	}
+
+
+	public Spielfigur[][] getZielFeldArray() {
+		return zielFeldArray;
+	}
+
 	
 	
 
