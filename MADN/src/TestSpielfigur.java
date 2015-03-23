0df2a92a07f6f3fc079aacfe2854316bc9694b88
SandyLoan <SandyLoan@Sandy-PC>
2015-03-23 17:08:00 +0100

    Testklasse von Spielfigur

---
+++ MADN/src/TestSpielfigur.java
@@ -0,0 +1,14 @@
+/**
+ * Die Klasse Testspielfigur 
+ * Spielfigur geht vor
+ **/
+public class TestSpielfigur {
+
+	public static void main(String[] args) {
+		Spielfigur fig= new Spielfigur(FarbEnum.ROT);
+		//fig.vor();
+		
+
+	}
+
+}
