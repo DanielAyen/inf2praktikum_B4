741068844fdb1fbfa71788fc9124c1c0709ec905
Elenis8 <eleni.stavrakis@student.reutlingen-university.de>
2015-03-24 12:33:37 +0100

    mehr Änderungen

--- MADN/src/Spielbrett.java
+++ MADN/src/Spielbrett.java
@@ -3,35 +3,53 @@ import java.util.ArrayList;
 /**
  * Die Klasse Spielbrett
  * 
+ * @param spielFeldArray
+ *            Array fuer alle normalen Spielfelder
+ * 
+ * @param startFeldArray
+ *            Array fuer Startfelder der Spieler
+ * 
+ * @param endFeldArray
+ *            Array fuer Endfelder der Spieler
  * 
  **/
 public class Spielbrett {
-	
-	
-	private Spielfeld   [] spielbrettArray = new Spielfeld [40];
-	private Spielfeld [][] startFeldArray = new Spielfeld [4][4];
-	private Spielfeld [][] zielFeldArray = new Spielfeld [4][4];
-	
-	private ArrayList <Object> FeldID = new ArrayList <Object>();
-	
-	public Spielbrett (Spielfeld feld, Spielfeld[] spielbrettArray,Spielfeld[][] startFeldArray, Spielfeld[][] zielFeldArray) {
-		
-		FeldID.add(new Spielfeld(this));
-		
-		this.spielbrettArray = spielbrettArray;
+
+	private Spielfeld[] spielFeldArray;
+	private Spielfeld[][] startFeldArray;
+	private Spielfeld[][] endFeldArray;
+
+	/**
+	 * Konstruktor des Spielbretts
+	 * 
+	 * 
+	 **/
+
+	public Spielbrett(Spielfeld[][] startFeldArray,
+			Spielfeld[][] zielFeldArray, Spielfeld[] spielFeldArray,
+			Spielfeld[][] endFeldArray) {
+
+		spielFeldArray = new Spielfeld[40];
+		for (int i = 0; i <= 39; i++) {
+			spielFeldArray[i] = new Spielfeld();
+		}
+
+		this.spielFeldArray = spielFeldArray;
+
+		startFeldArray = new Spielfeld[4][4];
+
+		for (int i = 0; i <= 3; i++) {
+			startFeldArray[i][i] = new Spielfeld();
+		}
 		this.startFeldArray = startFeldArray;
-		this.zielFeldArray = zielFeldArray;
+
+		endFeldArray = new Spielfeld[4][4];
+		for (int i = 0; i <= 3; i++) {
+			endFeldArray = new Spielfeld();
+		}
+		this.endFeldArray = endFeldArray;
+
 	}
 
-	public Spielfeld[] getSpielbrettArray() {
-		return spielbrettArray;
-	}
-
-	public Spielfeld [][] getStartFeldArray() {
-		return startFeldArray;
-	}
-
-	public Spielfeld[][] getZielFeldArray() {
-		return zielFeldArray;
-	}
 }
+
