1c169f25b66cff77aec23e6a9cc620e233c88b53
Verena <Verena@Verena-PC.fh-reutlingen.de>
2015-03-26 17:56:01 +0100

    neu erstellt

---
+++ MADN/src/KI_Aggressiv.java
@@ -0,0 +1,16 @@
+
+public class KI_Aggressiv extends KI {
+
+	public KI_Aggressiv(Spieler spieler) {
+		super(spieler);
+		// TODO Auto-generated constructor stub
+	}
+
+}
+/*schlagen
+ * raus
+ * beenden
+ * 
+ * laufen
+ * */
+ 
---
+++ MADN/src/KI_Defensiv.java
@@ -0,0 +1,18 @@
+
+public class KI_Defensiv extends KI {
+
+	public KI_Defensiv(Spieler spieler) {
+		super(spieler);
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+/*beenden
+ * raus
+ * schlagen
+ * 
+ * laufen 
+ * 
+ * */
+ 
