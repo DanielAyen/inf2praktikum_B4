4129d7aadeb96ce6c6e8a9c55359f8ec269218f8
DanielAyen <Daniel.Ayen@Student.Reutlingen-University.DE>
2015-03-30 13:20:35 +0200

    Test Test

--- MADN/src/SpielTest.java
+++ MADN/src/SpielTest.java
@@ -1,9 +1,22 @@
-
 public class SpielTest {
 
+	/**
+	 * Die Klasse SpielTest
+	 * 
+	 */
+
 	public static void main(String[] args) {
-	
 
+		Spielbrett brett = new Spielbrett();
+
+		Spieler spieler1 = new Spieler("Billy", FarbEnum.BLAU);
+		Spieler spieler2 = new Spieler("Fred", FarbEnum.GELB);
+		System.out.println(spieler1);
+		System.out.println(spieler2);
+
+		// spieler1.nochmalWuerfeln();
+
+		// getSpieler();
+		//figurenEinerFarbeAufFeld();
 	}
-
 }
