55c63a00996281b8c54c4ccc30b22690897f6ee4
SandyLoan <SandyLoan@Sandy-PC>
2015-03-30 11:41:19 +0200

    iBediener implementiert mit JavaDoc

--- MADN/src/iBediener.java
+++ MADN/src/iBediener.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
+
+/**
+ * über das Interface iBediener
+ * */
 
 public interface iBediener {
+
+	ArrayList<Spieler> getSpieler();
+
+	Spieler aktiverSpieler();
+
+	void zaehlerSpielerMax(int spielerAnz);
+
+	Spieler reihenfolgeSpieler();
+
+	void nochmalWuerfeln();
+
+	void entscheidungBeiSechs();
+
+	int aufStartSpielfeld();
+
+	void gegnerSchlagen();
+
+	boolean istFeldBelegt();
+
+	void welcheSpielfigurNutzen();
+
+	boolean istZielfeldEndfeld();
+
+	int figurenEinerFarbeAufFeld();
 
 }
