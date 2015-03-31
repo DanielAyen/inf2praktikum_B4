20cccc91aea94c4c1349f6023f4f8ffbf8357601
SandyLoan <SandyLoan@Sandy-PC>
2015-03-31 13:41:12 +0200

    änderung

--- MADN/src/SpielTest.java
+++ MADN/src/SpielTest.java
@@ -8,16 +8,18 @@ public class SpielTest {
 	public static void main(String[] args) {
 
 		Spielbrett brett = new Spielbrett();
-
+		
 		Spieler spieler1 = new Spieler("Billy", FarbEnum.BLAU);
 		Spieler spieler2 = new Spieler("Fred", FarbEnum.GELB);
 		System.out.println(spieler1);
 		System.out.println(spieler2);
 
 		spieler1.wuerfeln();
-		spieler2.nochmalWuerfeln();
+		spieler2.wuerfeln();
+		
+		//spielStarten();
 
-//		 getSpieler();
-		//figurenEinerFarbeAufFeld();
+        
+		
 	}
 }
