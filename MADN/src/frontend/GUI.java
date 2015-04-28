a4cffafb03b54acc65c09b72ac9ec39834a6bc98
DanielAyen <Daniel.Ayen@Student.Reutlingen-University.DE>
2015-04-28 13:15:06 +0200

    GUI

--- MADN/src/frontend/GUI.java
+++ MADN/src/frontend/GUI.java
@@ -20,7 +20,9 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
@@ -157,6 +159,9 @@ public class GUI extends JFrame implements ActionListener {
 				spAnzahl = ((Number) spinner.getValue()).intValue();
 				for (int i = 0; i < spAnzahl; i++) {
 					nameFarbeArtAbfrage();
+					
+					 nameFarbeArtAbfrage(); 
+					
 				} // oeffnet dann den neuen frame
 				frame.dispose(); // schliesst den frame beim klick auf OK button
 			}
@@ -165,115 +170,112 @@ public class GUI extends JFrame implements ActionListener {
 		frame.setVisible(true);
 	}
 
-	public void nameFarbeArtAbfrage() {
+	
 
-	}
-
-	// public void nameFarbeArtAbfrage() {
-	//
-	// JFrame frame02 = new JFrame("Spieler erstellen");
-	// frame02.setLayout(new GridLayout(2, 0));
-	// frame02.setSize(400, 210);
-	// frame02.setResizable(false);
-	// JLabel label02 = new JLabel(
-	// "Gebe deinen Spielernamen deine Farbe und die Art ein.");
-	// JPanel panel02 = new JPanel(new BorderLayout());
-	// JButton button02 = new JButton("Bestätigen");
-	// JTextField feld01 = new JTextField("Spielername");
-	//
-	// panel02.add(label02, BorderLayout.NORTH);
-	// label02.setLayout(new BorderLayout());
-	// panel02.add(button02, BorderLayout.SOUTH);
-	// button02.addActionListener(this);
-	//
-	// panel02 = new JPanel();
-	// panel02.setLayout(new FlowLayout());
-	// panel02.add(feld01, BorderLayout.SOUTH);
-	// frame02.add(label02, BorderLayout.NORTH);
-	// frame02.add(panel02, BorderLayout.SOUTH);
-	//
-	// //RadioButtons für Farbe
-	// final JRadioButton RBRED = new JRadioButton("RED", true);
-	// final JRadioButton RBGREEN = new JRadioButton("GREEN");
-	// final JRadioButton RBBLUE = new JRadioButton("BLUE");
-	// final JRadioButton RBYELLOW = new JRadioButton("YELLOW");
-	//
-	//
-	// ButtonGroup group = new ButtonGroup();
-	// group.add(RBRED);
-	// group.add(RBGREEN);
-	// group.add(RBBLUE);
-	// group.add(RBYELLOW);
-	//
-	// panel02.add(RBRED);
-	// panel02.add(RBGREEN);
-	// panel02.add(RBBLUE);
-	// panel02.add(RBYELLOW);
-	//
-	//
-	//
-	// //RadioButtons für Art
-	// final JRadioButton Mensch = new JRadioButton("Mensch", true);
-	// final JRadioButton KIa = new JRadioButton("KI-Aggressiv");
-	// final JRadioButton KId = new JRadioButton("KI-Defensiv");
-	//
-	// ButtonGroup group2 = new ButtonGroup();
-	// group2.add(Mensch);
-	// group2.add(KIa);
-	// group2.add(KId);
-	//
-	// panel02.add(Mensch);
-	// panel02.add(KIa);
-	// panel02.add(KId);
-	// panel02.add(button02, BorderLayout.SOUTH);
-	//
-	//
-	// spielerName = feld01.getText(); //gibt den eingegebenen Spielernamen aus
-	//
-	//
-	//
-	//
-	// button02.addActionListener(new ActionListener() {
-	// public void actionPerformed(ActionEvent ae) {
-	// //Radio Button Abfrage Farbe
-	// if( RBRED.isSelected() == true){
-	// spielerFarbe = "RED";
-	// }
-	// if( RBGREEN.isSelected() == true){
-	// spielerFarbe = "GREEN";
-	// }
-	// if( RBBLUE.isSelected() == true){
-	// spielerFarbe = "BLUE";
-	// }
-	// if( RBYELLOW.isSelected() == true){
-	// spielerFarbe = "YELLOW";
-	// }
-	//
-	// // Radio Button Abfrage Art
-	// if( Mensch.isSelected() == true){
-	// spielerArt= null; //null, da mensch keine ki ist
-	// }
-	// if( KIa.isSelected() == true){
-	// spielerArt = "AGGRESSIV";
-	// }
-	// if( KId.isSelected() == true){
-	// spielerArt= "DFENSIV" ;
-	// }
-	//
-	// nameFarbeArtUebergeben(); // beim klick auf den button werden die daten
-	// uebergeben
-	//	
-	//	
-	// if(s.getA() == false)
-	// {
-	// frame02.dispose();
-	// } // schliesst den frame beim klick auf button
-	// }
-	// }
-	// );
-	//
-	// frame02.setVisible(true);
-	// }
+	 public void nameFarbeArtAbfrage() {
+	
+	 JFrame frame02 = new JFrame("Spieler erstellen");
+	 frame02.setLayout(new GridLayout(2, 0));
+	 frame02.setSize(400, 210);
+	 frame02.setResizable(false);
+	 JLabel label02 = new JLabel(
+	 "Gebe deinen Spielernamen deine Farbe und die Art ein.");
+	 JPanel panel02 = new JPanel(new BorderLayout());
+	 JButton button02 = new JButton("Bestätigen");
+	 JTextField feld01 = new JTextField("Spielername");
+	
+	 panel02.add(label02, BorderLayout.NORTH);
+	 label02.setLayout(new BorderLayout());
+	 panel02.add(button02, BorderLayout.SOUTH);
+	 button02.addActionListener(this);
+	
+	 panel02 = new JPanel();
+	 panel02.setLayout(new FlowLayout());
+	 panel02.add(feld01, BorderLayout.SOUTH);
+	 frame02.add(label02, BorderLayout.NORTH);
+	 frame02.add(panel02, BorderLayout.SOUTH);
+	
+	 //RadioButtons für Farbe
+	 final JRadioButton RBRED = new JRadioButton("RED", true);
+	 final JRadioButton RBGREEN = new JRadioButton("GREEN");
+	 final JRadioButton RBBLUE = new JRadioButton("BLUE");
+	 final JRadioButton RBYELLOW = new JRadioButton("YELLOW");
+	
+	
+	 ButtonGroup group = new ButtonGroup();
+	 group.add(RBRED);
+	 group.add(RBGREEN);
+	 group.add(RBBLUE);
+	 group.add(RBYELLOW);
+	
+	 panel02.add(RBRED);
+	 panel02.add(RBGREEN);
+	 panel02.add(RBBLUE);
+	 panel02.add(RBYELLOW);
+	
+	
+	
+	 //RadioButtons für Art
+	 final JRadioButton Mensch = new JRadioButton("Mensch", true);
+	 final JRadioButton KIa = new JRadioButton("KI-Aggressiv");
+	 final JRadioButton KId = new JRadioButton("KI-Defensiv");
+	
+	 ButtonGroup group2 = new ButtonGroup();
+	 group2.add(Mensch);
+	 group2.add(KIa);
+	 group2.add(KId);
+	
+	 panel02.add(Mensch);
+	 panel02.add(KIa);
+	 panel02.add(KId);
+	 panel02.add(button02, BorderLayout.SOUTH);
+	
+	
+	 spielerName = feld01.getText(); //gibt den eingegebenen Spielernamen aus
+	
+	
+	
+	
+	 button02.addActionListener(new ActionListener() {
+	 public void actionPerformed(ActionEvent ae) {
+	 //Radio Button Abfrage Farbe
+	 if( RBRED.isSelected() == true){
+	 spielerFarbe = "RED";
+	 }
+	 if( RBGREEN.isSelected() == true){
+	 spielerFarbe = "GREEN";
+	 }
+	 if( RBBLUE.isSelected() == true){
+	 spielerFarbe = "BLUE";
+	 }
+	 if( RBYELLOW.isSelected() == true){
+	 spielerFarbe = "YELLOW";
+	 }
+	
+	 // Radio Button Abfrage Art
+	 if( Mensch.isSelected() == true){
+	 spielerArt= null; //null, da mensch keine ki ist
+	 }
+	 if( KIa.isSelected() == true){
+	 spielerArt = "AGGRESSIV";
+	 }
+	 if( KId.isSelected() == true){
+	 spielerArt= "DFENSIV" ;
+	 }
+	
+	 nameFarbeArtUebergeben(); // beim klick auf den button werden die daten uebergeben
+		
+		
+	 if(s.getA() == false)
+	 {
+	 frame02.dispose();
+	 } // schliesst den frame beim klick auf button
+	 }
+	 }
+	 );
+	
+	 frame02.setVisible(true);
+	 }
 
 	/* ___________Methoden____________ */
 	public void spielLaden() {
@@ -337,12 +339,21 @@ public class GUI extends JFrame implements ActionListener {
 
 		JLabel wuerfelL = new JLabel(eins); // so gibt er immer bild 1 aus
 
+		JPanel logger =new JPanel();
+		logger.setLayout(new BorderLayout());
+		JTextArea textArea=new JTextArea(10,10);
+		textArea.setEditable(false);
+		textArea.setMaximumSize(getSize());
+		// unsichtbar textArea.setOpaque(false);
+		logger.add(new JScrollPane(textArea),BorderLayout.CENTER);
+		
+		
 		// JLabel wuerfelL = new JLabel(zwei);
 		// JLabel wuerfelL = new JLabel(drei);
 		// JLabel wuerfelL = new JLabel(vier);
 		// JLabel wuerfelL = new JLabel(fuenf);
 		// JLabel wuerfelL = new JLabel(sechs);
-
+		hauptf.add(textArea, BorderLayout.SOUTH);
 		hauptf.add(wuerfelL);
 		hauptf.add(wuerfelL, BorderLayout.WEST);
 
