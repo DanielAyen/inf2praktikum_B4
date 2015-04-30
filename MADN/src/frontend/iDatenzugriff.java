574c09e6f822e9f3786f48bca0c7c6f059557ad4
Verena <Verena@Verena-PC.fritz.box>
2015-04-30 12:24:45 +0200

    PDF

--- MADN/src/frontend/iDatenzugriff.java
+++ MADN/src/frontend/iDatenzugriff.java
@@ -1,6 +1,7 @@
 package frontend;
 
 import java.io.IOException;
+import java.util.Properties;
 
 
 
@@ -10,6 +11,11 @@ public interface iDatenzugriff {
 	void speichern(String dateiname, String dateiende, Object o) throws IOException;
 	
 	Object laden(String dateiname);
+	
+	//void oeffnen(Properties p) throws IOException;
+	//void schreiben(Object object) throws IOException; --> speichern
+	//Object lesen() throws IOException; --> laden
+	//void schliessen(Object object) throws IOException;
 
 
 }
