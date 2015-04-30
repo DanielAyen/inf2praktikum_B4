26e44a24bedd480dc0d8479b37ea234e378a264c
Verena <Verena@Verena-PC.fritz.box>
2015-04-30 12:17:30 +0200

    neue Klasse erstellt

---
+++ MADN/src/backend/DatenzugriffPDF.java
@@ -0,0 +1,22 @@
+package backend;
+
+import java.io.IOException;
+
+import frontend.iDatenzugriff;
+
+public class DatenzugriffPDF implements iDatenzugriff {
+
+	@Override
+	public void speichern(String dateiname, String dateiende, Object o)
+			throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object laden(String dateiname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
